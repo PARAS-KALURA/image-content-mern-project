03f4fe7869862998e5f060d9ca32ac7c60c0a9d2
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-02-22 13:07:39 +0530

    Deleted Successfully ✅

--- src/app.js
+++ src/app.js
@@ -51,4 +51,29 @@ app.get("/posts", async (req, res) => {
     }
 } )
 
+app.delete("/posts/:id", async (req, res) => {
+    try {
+ 
+        const {id} = req.params;
+
+        const deletedPost = await postModel.findByIdAndDelete(id);
+
+        if(!deletedPost) {
+            return res.status(404).json({
+                message: "Post not found"
+            })
+        }
+
+        return res.status(200).json({
+            message: "Deleted Sucessfully"
+        })
+        
+
+    } catch(error) {
+        res.status(500).json({
+            message: "Server Error"
+        })
+    }
+} )
+
 module.exports = app;
